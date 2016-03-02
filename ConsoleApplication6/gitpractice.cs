75a7d2f1f6fa6046e8422f21f5c7083c74cd5b78
Nikita Rai <nikita.collier@gmail.com>
2016-03-02 10:09:07 -0500

    setting up repo

---
+++ ConsoleApplication6/gitpractice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 20;
+            int c = a % b;
+
+            Console.WriteLine(c);
+          
+        }
+    }
+}
